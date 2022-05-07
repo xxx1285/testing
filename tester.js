3aa28c3375a1fa263c71ee0628117bccd3ee0528
Alex18 <00018ds@gmail.com>
2022-05-07 19:35:56 +0300

    urok2

--- tester.js
+++ tester.js
@@ -1,13 +1,23 @@
 'use strict';
-const numberofFilms = promt('Ckolko filmov posmotrel?', '22');
+const numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', '22');
 
 const personalMovieDB = {
-   count: "",
-   movies: "",
-   actors: "",
-   genres: "",
-   privat: ""
+   count: numberofFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
 };
+let movies = {};
 
-console.log(numberofFilms);
+let a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies.a = b;
+personalMovieDB.movies.c = d;
+
+
+console.log(personalMovieDB);
 
