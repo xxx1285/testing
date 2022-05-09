8bd7541c1e12efb7ad7c6a1a05c1a62524be6dc5
Alex18 <00018ds@gmail.com>
2022-05-09 18:35:45 +0300

    komit

--- tester.js
+++ tester.js
@@ -48,25 +48,46 @@
 //    console.log(`okey ${i}`);
 //    qwerty++;
 // }
+
 let numberOfFilms = +prompt(`Skolko smotrel filmov?`, ``)
 
 const personalMovieDB = {
    count: numberOfFilms,
    movies: {},
-   privat: false
+   privat: true
 };
 
-for (let i = 0; i < 2; i++) {
-   let a = prompt(`Питання 1 захodi ${i}`, ``),
-       b = prompt(`2 HELP ${i}`, ``);
-   let c = 111;
-   if (a != null && b != null && a != '' && b != '') {
-      personalMovieDB.movies[a] = b;
-      console.log('done');
-   } else {
-      console.log('error');
-      i--;
-   };
+// for (let i = 0; i < 2; i++) {
+//    let a = prompt(`Питання 1 захodi ${i}`, ``),
+//        b = prompt(`2 HELP ${i}`, ``);
+//    let c = 111;
+//    if (a != null && b != null && a != '' && b != '') {
+//       personalMovieDB.movies[a] = b;
+//       console.log('done');
+//    } else {
+//       console.log('error');
+//       i--;
+//    };
    
-   console.log(personalMovieDB);
+// };
+
+function showMyDB (hidden) {
+   if (!hidden) {
+      console.log(personalMovieDB);
+   };
 };
+
+showMyDB(personalMovieDB.privat);
+
+
+// function roughScale(x, base) {
+//    const parsed = parseInt(x, base);
+//    if (isNaN(parsed)) { return 0; }
+//    return parsed * 100;
+//  }
+ 
+//  console.log(roughScale(' 0xF', 16));
+//  // expected output: 1500
+ 
+//  console.log(roughScale('321', 2));
+ // expected output: 0
